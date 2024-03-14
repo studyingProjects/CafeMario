f5ad75f8b2dcf8413d5973f0f842ab8fe3e2ebd4
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-03-14 11:59:16 +0300

    Initial Commit

---
+++ CafeMario/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CafeMario
+//
+//  Created by Andrei Shpartou on 14/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

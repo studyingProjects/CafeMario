7d51474764aeb492671423fdd76851d658dead7f
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-03-16 10:29:52 +0300

    add UIColor extensions

---
+++ CafeMario/Extensions/UIColor+App.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+App.swift
+//  CafeMario
+//
+//  Created by Andrei Shpartou on 16/03/2024.
+//
+
+import UIKit
+
+extension UIColor {
+    static var customRed: UIColor {
+        UIColor(red: 247, green: 95, blue: 106, alpha: 1)
+    }
+
+    static var customGreen: UIColor {
+        UIColor(red: 93, green: 184, blue: 92, alpha: 1)
+    }
+}
--- CafeMario/Scenes/Login/View/LoginView.swift
+++ CafeMario/Scenes/Login/View/LoginView.swift
@@ -8,4 +8,20 @@
 import UIKit
 
 class LoginView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupViews() {
+    }
+}
+
+// MARK: - Constraints
+private extension LoginView {
+    private func setupConstraints() {
+    }
 }
